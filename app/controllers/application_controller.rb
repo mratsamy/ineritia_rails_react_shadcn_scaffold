3a289f2912c15f6c0ba238bb1fb2aa7e371c6eda
Michael Ratsamy <michael.ratsamy@gmail.com>
2025-07-16 12:26:36 -0700

    adds current_user method to auth and updates application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   # Allows for sharing the props across all pages
   inertia_share do
     {
-      current_user: current_user,
-      notifications: current_user&.unread_notifications_count
-    } if user_signed_in?
+      current_user: current_user
+    }
   end
 end
--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -17,6 +17,10 @@ module Authentication
       resume_session
     end
 
+    def current_user
+      @current_user ||= resume_session
+    end
+
     def require_authentication
       resume_session || request_authentication
     end
