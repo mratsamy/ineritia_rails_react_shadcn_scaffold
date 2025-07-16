27ab6a3353dc703761a413719fb7d8a5c6bbc2dc
Michael Ratsamy <michael.ratsamy@gmail.com>
2025-07-16 10:15:24 -0700

    runs the rails 8 authentication generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Allows for sharing the props across all pages
+  inertia_share do
+    {
+      current_user: current_user,
+      notifications: current_user&.unread_notifications_count
+    } if user_signed_in?
+  end
 end
